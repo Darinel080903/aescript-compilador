4e3e4d5ecb22fe19bf8e716a43299d43a3d67920
Darinel Monjaras Ruiz <darinelmr12345@gmail.com>
2024-04-07 21:19:52 -0600

    project finished

---
+++ generate_code.py
@@ -0,0 +1,116 @@
+import re
+
+python_code = ""
+variables = []
+value_variables = []
+functions = []
+for_result = []
+function_bodies = {}
+
+
+def operators(op, left, right):
+    if op == '>':
+        return int(left) > int(right)
+    elif op == '<':
+        return int(left) < int(right)
+
+
+def generate_python_code(parsed_output):
+    global python_code, variables, value_variables, functions
+    for statement in parsed_output:
+        print(statement)
+        if statement[0] == 'print_statement':
+            python_code = ""
+            print(statement[1])
+            if type(statement[1]) == int: #imprimir(1);
+                python_code += f"{statement[1]}"
+            elif type(statement[1]) == str: #imprimir("hola"); | imprimir(suma)
+                print("ENTRE")
+                print(statement[1])
+                if statement[1] in variables:
+                    index = variables.index(statement[1])
+                    python_code += f"{value_variables[index]}"
+                elif statement[1][0] == '"':
+                    python_code += f"{statement[1]}"
+                else:
+                    python_code += f"VARIABLE NO DEFINIDA"
+            elif type(statement[1]) == tuple:
+                try:
+                    if statement[1][1] in variables and statement[1][2]:  # suma + suma;
+                        print("Entre3")
+                        index1 = variables.index(statement[1][1])
+                        index2 = variables.index(statement[1][2])
+                        python_code += f"{value_variables[index1]} {statement[1][0]} {value_variables[index2]}"
+                    elif statement[1][1][0] == '"' and statement[1][2][0] == '"':
+                        print("Entre4")
+                        python_code += f"{statement[1][1]}{statement[1][2]}"
+                    elif statement[1][1] in variables and statement[1][2][0] == '"':
+                        match = re.search(r'\".*?\"', statement[1][1])
+                        print("Entre5")
+                        index = variables.index(match.group(0)[1:-1])
+                        python_code += f"{value_variables[index]}{statement[1][2]}"
+                    elif statement[1][1][0] == '"' and statement[1][2] in variables:
+                        match = re.search(r'\".*?\"', statement[1][1])
+                        print("Entre6")
+                        index = variables.index(statement[1][2])
+                        python_code += f"{match.group(0)[1:-1]}{value_variables[index]}"
+                    elif type(statement[1][1]) == int and type(statement[1][2]) == int:  # 1 + 1;
+                        print("Entre7")
+                        python_code += f"{statement[1][1]} {statement[1][0]} {statement[1][2]}"
+                    else:
+                        python_code += f"VARIABLE NO DEFINIDA"
+                except:
+                    python_code += f"ERROR EN COMPATIBILIDAD DE TIPOS"
+        elif statement[0] == 'declaration':
+            print(f"flat{statement[1]}")
+            print(variables)
+            python_code = ""
+            if statement[1] in variables:
+                print("hay doble")
+                python_code += "VARIABLE DOBLE"
+                break
+            else:
+                variables.append(statement[1])
+                if type(statement[2]) != int:
+                    match = re.search(r'\".*?\"', statement[2])
+                    value_variables.append(match.group(0)[1:-1])
+                else:
+                    value_variables.append(statement[2])
+        elif statement[0] == 'function_declaration':
+            print("FUNCTION DECLARATION")
+            print(statement[2])
+            if statement[2] == 'correr':
+                generate_python_code(statement[4])
+            else:
+                python_code = ""
+                functions.append(statement[1])
+                function_bodies[statement[1]] = statement[3]
+        elif statement[0] == 'iterate_statement':
+            for_result.clear()
+            for i in range(statement[1]):
+                generate_python_code(statement[2])
+                for_result.append(python_code)
+        elif statement[0] == 'conditional_statement':
+            if statement[1] in variables:
+                left = value_variables[variables.index(statement[1])]
+            else:
+                left = statement[1]
+            if statement[3] in variables:
+                right = value_variables[variables.index(statement[3])]
+            else:
+                right = statement[3]
+            if operators(statement[2], left, right):
+                new_statement = [statement[4]]
+                generate_python_code(new_statement)
+            else:
+                python_code = "CARACTERES INCORRECTOS"
+        elif statement[0] == 'call_function':
+            print(functions)
+            if statement[1] in functions:
+                generate_python_code(function_bodies[statement[1]])
+            else:
+                python_code = "FUNCION NO DEFINIDA"
+    if len(for_result) > 0:
+        return for_result
+    else:
+        return python_code
---
+++ ui.py
@@ -0,0 +1,71 @@
+import tkinter as tk
+
+import sintaxis
+from generate_code import generate_python_code
+from lexer import lexer
+import re
+
+from sintaxis import parser
+
+
+class IDESimulator:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Simple IDE Simulator")
+        self.text_widget = tk.Text(root, height=15, width=50)
+        self.text_widget.pack()
+        self.validate_button = tk.Button(root, text="Validate", command=self.validate_code)
+        self.validate_button.pack()
+        self.result_text = tk.Text(root, height=10, width=50)
+        self.result_text.pack()
+        self.console_text = tk.Text(root, height=10, width=50)
+        self.console_text.pack()
+
+
+    def validate_code(self):
+        self.result_text.delete("1.0", tk.END)
+        self.console_text.delete("1.0", tk.END)
+        self.console_text.insert(tk.END, "Console output:\n")
+        code = self.text_widget.get("1.0", tk.END)
+        code = code.replace("\n", "")
+        tokens_info = self.analyze_code(code)
+        parsed_output = parser.parse(code)
+        if sintaxis.error_message is not None:
+            parser_result = sintaxis.error_message
+            self.result_text.insert(tk.END, f"Parser result: {parser_result}\n")
+            sintaxis.error_message = None
+        else:
+            parser_result = "No se encontraron errores de sintaxis."
+            self.result_text.insert(tk.END, f"Parser result: {parser_result}\n")
+            self.result_text.insert(tk.END, "Lexer result:\n")
+            for token_info in tokens_info:
+                self.result_text.insert(tk.END, f"{token_info}\n")
+        python_code = generate_python_code(parsed_output)
+        if type(python_code) == list:
+            self.result_text.insert(tk.END, "\n")
+            for code in python_code:
+                fl_text = eval(code)
+                self.console_text.insert(tk.END, f"{fl_text}\n")
+        else:
+            try:
+                final_code = eval(python_code) #"No se declaro";
+                print(final_code)
+                self.console_text.insert(tk.END, f"\n{final_code}")
+            except:
+                self.console_text.insert(tk.END, f"\n{python_code}")
+
+    def analyze_code(self, code):
+        lexer.input(code)
+        tokens_info = []
+        while True:
+            tok = lexer.token()
+            if not tok:
+                break
+            tokens_info.append(f"Token: {tok.type}, Value: {tok.value}")
+        return tokens_info
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    ide = IDESimulator(root)
+    root.mainloop()
